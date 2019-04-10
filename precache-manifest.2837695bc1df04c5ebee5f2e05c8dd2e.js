f05d9d5f1172344ee1bb6912d57c4ed7719ced57
Artem <artem.kaseka@gmail.com>
2019-04-10 13:12:49 +0300

    Updates

---
+++ precache-manifest.2837695bc1df04c5ebee5f2e05c8dd2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "28dc38fd197e8f2a8b1c",
+    "url": "/react-test-task/static/css/main.df3d07d7.chunk.css"
+  },
+  {
+    "revision": "28dc38fd197e8f2a8b1c",
+    "url": "/react-test-task/static/js/main.f08a3226.chunk.js"
+  },
+  {
+    "revision": "ebb52c66c62f2ce7055a",
+    "url": "/react-test-task/static/js/runtime~main.d7452bba.js"
+  },
+  {
+    "revision": "a8379b34d85edc3ca64f",
+    "url": "/react-test-task/static/css/2.ab5a8158.chunk.css"
+  },
+  {
+    "revision": "a8379b34d85edc3ca64f",
+    "url": "/react-test-task/static/js/2.7959c475.chunk.js"
+  },
+  {
+    "revision": "888360c6f49a5625886ee21097b353d2",
+    "url": "/react-test-task/static/media/hero_bg.888360c6.jpg"
+  },
+  {
+    "revision": "a1fef822f39f6544e69e5e3e60cc9ce9",
+    "url": "/react-test-task/index.html"
+  }
+];
